cfe9fa1588f03ddb68c092152be639efeeebece4
AHQ Miness <ahqsecret@gmail.com>
2024-12-24 20:33:10 +0530

    no suffic

--- ver.js
+++ ver.js
@@ -5,9 +5,7 @@ const version =
   process.env.VERSION ||
   `${date.getFullYear()}.${date.getMonth() == 0 ? date.getMonth() + 1 : 0}.${
     date.getDate() == 1 ? 0 : date.getDate()
-  }` +
-    (process.env.NIGHTLY == "true" ? `-nightly.${Date.now()}` : "") +
-    suffix;
+  }` + (process.env.NIGHTLY == "true" ? `-nightly.${Date.now()}` : "");
 
 console.log(process.env.VERSION || `Created version ${version}`);
 
